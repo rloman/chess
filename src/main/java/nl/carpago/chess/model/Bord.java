82a650dcf2c96c50307db88f5ec1ef7f2e8186ac
Carpago Software | Raymond Loman <raymond@carpago.nl>
2014-09-24 16:52:39 +0200

    Nice matrix print

--- src/main/java/nl/carpago/chess/model/Bord.java
+++ src/main/java/nl/carpago/chess/model/Bord.java
@@ -2,18 +2,22 @@ package nl.carpago.chess.model;
 
 public class Bord {
 
-	private final int	SIZE	= 8;
+	private final int SIZE = 8;
 
-	private int[][]		bord	= new int[SIZE][SIZE];
+	private int[][] bord = new int[SIZE][SIZE];
 
 	public int getSize() {
 		return SIZE;
 	}
 
 	public void toon() {
+		toonAlfabet();
 		toonHeaderFooter();
+		int currentIndexVerticaal = 8;
 		for (int[] horizontaal : bord) {
+			System.out.print(currentIndexVerticaal--);
 			for (int verticaal : horizontaal) {
+
 				System.out.print("|" + verticaal);
 
 			}
@@ -22,15 +26,24 @@ public class Bord {
 		toonHeaderFooter();
 	}
 
+	private void toonAlfabet() {
+		System.out.print("  ");
+		for (Lokatie locatie : Lokatie.values()) {
+			System.out.print(locatie + " ");
+		}
+		System.out.println();
+	}
+
 	private void toonHeaderFooter() {
 		final String separator = "=";
+		System.out.print(" ");
 		for (int i = 0; i < 8; i++) {
 			System.out.print(separator + separator);
 		}
 		System.out.println(separator);
 	}
 
-	public void setOccupied(Positie pos){
+	public void setOccupied(Positie pos) {
 		this.bord[pos.getxCoord()][pos.getyCoord()] = 1;
 	}
 }
